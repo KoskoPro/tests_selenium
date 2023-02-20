999b0428af952c6ecfa64de55bbe9f5d86be8fb1
Kosko <xanaxko@gmail.com>
2023-02-20 01:51:06 +0100

    Adding test_items and conftest

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button_addtobasket(browser):
+    browser.get(link)
+    time.sleep(10)
+    button = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-bask')
+    assert len(button) > 0, "Button 'Add to Basket' not founded"
